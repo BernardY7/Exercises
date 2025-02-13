dbe20050d5e58b2f52a213b65d94f8ffdfb34d4e
Bernard <bernardyoussef123@gmail.com>
2025-02-12 20:54:11 -0800

    ex1

---
+++ exercises.py
@@ -0,0 +1,10 @@
+#ex1 
+age = int(input(" Please enter your age: "))
+if age < 10:
+    print(" The ticket price is $5.")
+
+elif age > 10 and age < 18:
+    print(" The ticket price is $10.")
+
+elif age > 18:
+    print(" The ticket price is $15.")
